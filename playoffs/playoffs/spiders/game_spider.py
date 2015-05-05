1dbe74324db5b83b10da7754a8ada395c4dcbb94
kielejocain <joecken@gmail.com>
2015-05-04 19:46:25 -0700

    scraper works with current teams + PHO + ATL

--- playoffs/playoffs/spiders/game_spider.py
+++ playoffs/playoffs/spiders/game_spider.py
@@ -35,9 +35,47 @@ class ScheduleSpider(CrawlSpider):
         # collect xpaths of each team (row in table)
         rows = sel.xpath('/html//div[@class="contentBlock"]/table/tbody/tr')
 
+        # define collection of teams that play in games we care about
+        # (some teams have moved, but we still want the old games)
+        # We'll grab the first four letters of the team to distinguish,
+        # then assign the NHL's standard 3-letter code.
+
+        TEAMS = {'Anah': 'ANA',
+                 'Ariz': 'ARI',
+                 'Calg': 'CGY',
+                 'Edmo': 'EDM',
+                 'Los ': 'LAK',
+                 'San ': 'SJS',
+                 'Vanc': 'VAN',
+                 'Chic': 'CHI',
+                 'Colo': 'COL',
+                 'Dall': 'DAL',
+                 'Minn': 'MIN',
+                 'Nash': 'NAS',
+                 'St. ': 'STL',
+                 'Winn': 'WPG',
+                 'Bost': 'BOS',
+                 'Buff': 'BUF',
+                 'Detr': 'DET',
+                 'Flor': 'FLA',
+                 'Mont': 'MTL',
+                 'Otta': 'OTT',
+                 'Tamp': 'TBL',
+                 'Toro': 'TOR',
+                 'Caro': 'CAR',
+                 'Colu': 'CBJ',
+                 'New ': 'NJD',
+                 'NY I': 'NYI',
+                 'NY R': 'NYR',
+                 'Phil': 'PHI',
+                 'Pitt': 'PIT',
+                 'Wash': 'WAS',
+                 'Phoe': 'ARI',
+                 'Atla': 'WPG'}
+
         # loop through teams
         for row in rows:
-            if row.xpath('td[2]/a[1]/@rel').extract():
+            if row.xpath('td[1]/@colspan').extract()[0] == '1':
                 loader = ItemLoader(PlayoffsItem(), selector=row)
                 loader.default_input_processor = MapCompose()
                 loader.default_output_processor = Join()
@@ -48,29 +86,37 @@ class ScheduleSpider(CrawlSpider):
                 loader.add_value('date', str(date))
 
                 # get team identifiers
-                away = row.xpath('td[2]/a[1]/@rel').extract()[0]
-                loader.add_value('away', away)
-                home = row.xpath('td[3]/a[1]/@rel').extract()[0]
-                loader.add_value('home', home)
+                away = ''
+                if row.xpath('td[2]/a[1]/@rel').extract():
+                    away = row.xpath('td[2]/a[1]/@rel').extract()[0]
+                elif row.xpath('td[2]/div/text()').extract()[0][:4] in TEAMS:
+                    away = TEAMS[row.xpath('td[2]/div/text()').extract()[0][:4]]
+                if away:
+                    if row.xpath('td[3]/a[1]/@rel').extract():
+                        home = row.xpath('td[3]/a[1]/@rel').extract()[0]
+                    else:
+                        home = TEAMS[row.xpath('td[3]/div/text()').extract()[0][:4]]
+                    loader.add_value('away', away)
+                    loader.add_value('home', home)
 
-                # collect and parse results
-                away_score = row.xpath('td[5]/span[1]/text()').extract()[0].replace('\n', '').strip()
-                match = re.search('\(.*?\)', away_score)
-                loader.add_value('away_score', match.group(0)[1:-1])
-                home_score = row.xpath('td[5]/span[2]/text()').extract()[0].replace('\n', '').strip()
-                match = re.search('\(.*?\)', home_score)
-                loader.add_value('home_score', match.group(0)[1:-1])
-                match = re.search('\).*', home_score)
-                result = match.group(0)[1:]
-                if result == 'S/O':
-                    output = 'SO'
-                elif result == 'OT':
-                    output = 'OT'
-                else:
-                    output = 'REG'
-                loader.add_value('result', output)
+                    # collect and parse results
+                    away_score = row.xpath('td[5]/span[1]/text()').extract()[0].replace('\n', '').strip()
+                    match = re.search('\(.*?\)', away_score)
+                    loader.add_value('away_score', match.group(0)[1:-1])
+                    home_score = row.xpath('td[5]/span[2]/text()').extract()[0].replace('\n', '').strip()
+                    match = re.search('\(.*?\)', home_score)
+                    loader.add_value('home_score', match.group(0)[1:-1])
+                    match = re.search('\).*', home_score)
+                    result = match.group(0)[1:]
+                    if result == 'S/O':
+                        output = 'SO'
+                    elif result == 'OT':
+                        output = 'OT'
+                    else:
+                        output = 'REG'
+                    loader.add_value('result', output)
 
-                # feed item to pipeline
-                yield loader.load_item()
+                    # feed item to pipeline
+                    yield loader.load_item()
             else:
                 pass
